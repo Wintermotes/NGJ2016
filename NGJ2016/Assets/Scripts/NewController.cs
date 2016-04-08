cef9a85fc8907f1e1997b1e5522816e37af43f11
Jeppeh Olsen <jeppeholsen@h323.aau-1-day-2b.wlan.aalborguniversitet.dk>
2016-04-08 23:21:14 +0200

    Added folders

---
+++ NGJ2016/Assets/Scripts/NewController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewController : MonoBehaviour {
+
+	//Prefix for controller type
+	public string prefix;
+
+	//Buttons
+	private float stickHorizontal;
+	private float stickVertical;
+	private bool SQUARE;
+	private bool X;
+	private bool ROUND;
+	private bool TRIANGLE;
+
+
+
+	GameObject player;
+	Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = this.gameObject.GetComponent<Rigidbody> ();
+		player = this.gameObject;
+	}
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+
+	//Gets the input from the controller and maps it to variables
+	private void GetInputButtonValues(){
+		stickHorizontal = Input.GetAxis( prefix + "_Horizontal" );
+		stickVertical = Input.GetAxis (prefix + "_Vertical");
+		X = (Input.GetAxis (prefix + "_Jump") == 1) ? true : false;
+		SQUARE = (Input.GetAxis (prefix + "_Push") == 1) ? true : false;
+		TRIANGLE = (Input.GetAxis (prefix + "_Explode") == 1) ? true : false;
+		ROUND = (Input.GetAxis (prefix + "_Throw") == 1) ? true : false;
+	}
+}
