2778af108a74c60d6d5c004ef2a6a4d63ea399fe
Jeppeh Olsen <jeppeholsen@h323.aau-1-day-2b.wlan.aalborguniversitet.dk>
2016-04-09 16:42:15 +0200

    Implementing ragdoll controllers

--- NGJ2016/Assets/Scripts/PlayerController.cs
+++ NGJ2016/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NewController {
+
+//	GameObject leftHand;
+//	GameObject rightHand;
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("Player " + prefix);
+
+//		int children = transform.childCount;
+//		for (int i = 0; i < children; ++i) {
+//			Transform child = transform.GetChild (i);
+//			Debug.Log ("Tag = " + child.tag);
+//			if (child.tag == "LeftHand") {
+//				
+//				leftHand = child;
+//			}
+//			if (child.tag == "RightHand")
+//				rightHand = child;
+//
+//		}
+//
+//		print("leftHand:  " + leftHand);
+//		print("rightHand:  " + rightHand);
+			
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (GetLeftStick ().magnitude > 0) {
+			GameObject leftHand = GameObject.FindGameObjectWithTag (prefix + "_LeftHand");
+			leftHand.gameObject.GetComponent<Rigidbody2D> ().AddForce (GetLeftStick () * 10);
+		}
+
+		if (GetRightStick ().magnitude > 0) {
+			GameObject rightHand = GameObject.FindGameObjectWithTag (prefix + "_RightHand");
+			rightHand.gameObject.GetComponent<Rigidbody2D> ().AddForce (GetRightStick () * 10);
+		}
+
         if (Input.GetKey(KeyCode.E))
         {
             Debug.Log("Add force");
