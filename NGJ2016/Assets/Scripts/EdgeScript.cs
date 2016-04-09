93e843b2f1c2ff5d92de77d3f700ac65664d2ce3
Wintermotes <patri-kk@hotmail.com>
2016-04-09 09:05:22 +0200

    Floating, edge, initial sprite placeholers and sprites

---
+++ NGJ2016/Assets/Scripts/EdgeScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeScript : MonoBehaviour
+{
+    public float bounceEffect = 100.0f; 
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Vector2 forceVec = -other.GetComponent<Rigidbody2D>().velocity * bounceEffect;
+        other.GetComponent<Rigidbody2D>().AddForce(forceVec);
+    }
+}
