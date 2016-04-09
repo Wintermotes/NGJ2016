080527ddd2db3f568d8e7a6fdb41cf9b67732d04
Wintermotes <patri-kk@hotmail.com>
2016-04-10 00:30:21 +0200

    1st ragdoll

---
+++ NGJ2016/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("Add force");
+            GetComponent<Rigidbody2D>().AddForce(Vector2.left * 10.0f);
+        }
+	}
+}
