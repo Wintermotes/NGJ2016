f46840d5d119bd3838b15cad34f66a7d015f1510
Jeppeh Olsen <jeppeholsen@a.local>
2016-04-09 02:14:03 +0200

    Added scripts and prefabs for hole, holemanager and levelManager

---
+++ NGJ2016/Assets/Scripts/HoleManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleManager : MonoBehaviour {
+
+	private ArrayList holeList = new ArrayList ();
+	public GameObject holeObject;
+
+	public void CreateNewHole (){
+
+		GameObject hole = Instantiate(holeObject) as GameObject; 
+		hole.GetComponent<Hole>().Init();
+		holeList.Add (hole);
+	}
+
+	private void UpdateHoleList(){}
+
+	public ArrayList GetHoleList(){
+		return holeList; 
+	}
+}
